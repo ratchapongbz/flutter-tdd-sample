a6ffadc2c432f3806980f56bdc7906030687a6d0
Ratchapong Prasertchartmora <ratchapong.p@umt.ltd>
2022-05-13 02:07:48 +0700

    feat: implement RemoteConfigLoginDatasource

--- lib/features/auth/data/datasources/remote_config_login_datasource.dart
+++ lib/features/auth/data/datasources/remote_config_login_datasource.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter_tdd_sample/features/auth/data/datasources/login_datasource.dart';
 import 'package:flutter_tdd_sample/features/auth/data/models/app_user_model.dart';
+import 'package:flutter_tdd_sample/features/common/errors/app_error.dart';
 import 'package:flutter_tdd_sample/features/common/responses/response.dart';
 
 class RemoteConfigLoginDatasource extends LoginDatasource {
@@ -8,8 +11,25 @@ class RemoteConfigLoginDatasource extends LoginDatasource {
 
   RemoteConfigLoginDatasource(this._remoteConfig);
 
+  static const String referenceKey = 'users';
+
   @override
-  Future<Response<AppUserModel>> loginWithUsernameAndPassword(String username, String password) {
-    throw UnimplementedError();
+  Future<Response<AppUserModel>> loginWithUsernameAndPassword(String username, String password) async {
+    await _remoteConfig.fetchAndActivate();
+    final userDataString = _remoteConfig.getString(referenceKey);
+    try {
+      Iterable decodedUsers = jsonDecode(userDataString);
+
+      final user = decodedUsers.firstWhere(
+        (element) => element['username'] == username && element['password'] == password,
+        orElse: () => null,
+      );
+
+      final parsedUser = user == null ? null : AppUserModel.fromJson(user);
+      final error = parsedUser != null ? null : AppError.unAuthorized('unauthorized');
+      return Response(value: parsedUser, error: error);
+    } catch (exception) {
+      return Response(value: null, error: AppError.nullValue('null value'));
+    }
   }
 }
